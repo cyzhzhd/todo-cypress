fc51c7ffcc5893f14b0e5db2222d7dc714c31e9f
Ehyun Kwon <cyzhzhd@gmail.com>
2021-07-24 14:43:04 +0900

    feat: remove todo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,12 +2,19 @@ import TodoCard from '../TodoCard/TodoCard';
 import './TodoList.css';
 
 const TodoList = (props) => {
+  const removeItem = (index) => {
+    const newTodos = [...props.todos];
+    newTodos.splice(index, 1);
+    props.setTodos(newTodos);
+  };
+
   return (
     <div className='todo-list'>
       {props.todos.map((todo, index) => {
         return (
           <div className='todo-card-wrapper'>
             <TodoCard name={todo} />
+            <button onClick={() => removeItem(index)}>X</button>
           </div>
         );
       })}
